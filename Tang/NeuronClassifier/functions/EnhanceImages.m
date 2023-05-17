0dcc9714355a9c4de92eb96c2e592f8c4bfe006a
Lingjie Tang <wasdiojk@126.com>
2023-05-17 10:43:35 +0900

    230517

--- Tang/NeuronClassifier/functions/EnhanceImages.m
+++ Tang/NeuronClassifier/functions/EnhanceImages.m
@@ -15,25 +15,45 @@ mydata = cell(1, numfiles);
 
 mkdir MatlabEnhanced
 
-for k = 3%1:numfiles
+for k = 5:10%1:numfiles
     mydata{k} = imread(TifFiles(k).name);
     I0 = mydata{k};
     I1 = imlocalbrighten(I0);
     I2 = imlocalbrighten(I1);
-    I3 = imlocalbrighten(I2);
-    I3 = imadjust(I3);
+    
+    I3 = imreducehaze(I2);
+
+
+   % I3 = imadjust(I3);
+    n = 1.2;  
+    Idouble = im2double(I3); 
+   avg = mean2(Idouble);
+   sigma = std2(Idouble);
+
+ I3 = imadjust(I3,[max(0,avg-n*sigma) avg+n*sigma],[]);
+   
+   
+    
+   
+    
+    %I3 = imlocalbrighten(I2);
+    % I2 = imlocalbrighten(I1);
+    
+    %I3 = imadjust(I2);
 
     I4 = imbinarize(I3,"adaptive");
-    I5 = bwareaopen(I4, 2500);
-    I6 = medfilt2(I5);
-    I7 = imclose(I6, strel('disk',2));
+    I5 = medfilt2(I4);
+
+    I6 = bwareaopen(I5, 100);
+    
+    I7 = imclose(I6, strel('disk',3));
     I7 =  medfilt2(I7);
-    I7 = bwareaopen(I7, 2500);
+    I7 = bwareaopen(I7, 7000);
     I8 = medfilt2(I7);
     
 
-  % imwrite(I3,['MatlabEnhanced\',TifFiles(k).name])
-  %  figure
+ % imwrite(I8,['MatlabEnhanced\',TifFiles(k).name])
+  figure
     montage({I0, I1, I2, I3,I4,I5,I6,I7,I8},'Size',[2,5])
 end
 
